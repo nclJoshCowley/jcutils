02a9e0b68cf6d6387e5fec5f694bd380a54bc02c
Josh Cowley <j.cowley1@ncl.ac.uk>
2021-03-28 17:35:09 +0100

    Expanded testdata to allow for multiple response including normal, binomial, poisson

--- data-raw/testdata.R
+++ data-raw/testdata.R
@@ -5,13 +5,14 @@
 # Parameters
 set.seed(23)
 n_s <- 20 # 150 # 1000
-coeff_sd <- 0.1
 
-testdata <- tibble::tibble(
+
+# Explanatory variables
+test_expl <- tibble::tibble(
   # Continuous, bell curve about 0
   expl_rnorm = rnorm(n_s, mean = 0, sd = 1),
   # Continuous, uniform about 0
-  expl_runif = runif(n_s, min = 1, max = 2),
+  expl_runif = runif(n_s, min = 0.5, max = 1.5),
   # Binary variable, values are T/F (could be handled different to 2-lvl factor)
   expl_bin = sample(c(TRUE, FALSE), size = n_s, replace = TRUE),
   # Factor variable, 3 levels
@@ -28,27 +29,52 @@ testdata <- tibble::tibble(
   expl_some_missing = ifelse(
     seq(n_s) %% 5 == 0,
     NA_real_,
-    rnorm(n_s, mean = 1, sd = 2)
+    rnorm(n_s, mean = 0.5, sd = 2)
   )
 )
 
-# Only 1 response variable for now
-coeff <- sample(c(seq(19), -seq(9)), size = ncol(testdata))
-attr(testdata, "coeff") <- coeff
+# FUN: Creates coefficients and stores them for later. See ?ff_create_coeffs
+create_coeffs <- ff_create_coeffs(size = ncol(test_expl))
 
-testdata <- tibble::add_column(
-  testdata,
-  resp_cts = Reduce(`+`, lapply(seq_along(testdata), function(j) {
-    noisey_coeff <- coeff[j] + rnorm(n_s, mean = 0, sd = coeff_sd)
-    x <- as.numeric(testdata[[j]])
-    return(ifelse(is.na(x), NA, noisey_coeff * x))
-  })),
-  resp_int = round(abs(resp_cts)),
-  resp_bin = sign(resp_cts) == 1,
-  .before = 1
+# FUN: Wrapper around above that gets a vector representing XB = \sum(X_i * b_i)
+create_lp <- function(...) {
+  Reduce(
+    f = `+`,
+    purrr::map2(test_expl, create_coeffs(...), ~ as.numeric(.x) * .y)
+  )
+}
+
+# Response variables
+test_resp <- tibble::tibble(
+  # Y = XB (1)
+  resp_lp1 = create_lp(),
+  # Y = XB (2)
+  resp_lp2 = create_lp(),
+  # Y = exp(XB)  ...  (Y > 0)
+  resp_exp = create_lp(vals = seq(0.01, 1.5, 0.01), pm = FALSE, std_dev = 0.01) %>%
+    exp(),
+  # Y = round(exp(XB))  ...  (Y > 0, Y integer)
+  resp_pois = create_lp(vals = seq(0.01, 1, 0.01), pm = FALSE, std_dev = 0.01) %>%
+    exp() %>%
+    round(),
+  # Y = exp(XB) / (1 + exp(XB))  ..  (Y \in [0,1])
+  resp_ilogit = create_lp(vals = seq(0.01, 1, 0.01), std_dev = 0.01) %>%
+    sapply(function(.x) exp(.x) / (1 + exp(.x)) )
 )
 
-# Add to package
+
+# Format data
+testdata <- dplyr::bind_cols(test_resp, test_expl)
+
+coeffs <- rlang::env_get(environment(create_coeffs), "c_all") %>%
+  setNames(colnames(test_resp)) %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(expl = colnames(test_expl), .before = 1)
+attr(testdata, "coeffs") <- coeffs
+
+# Inspection by eye
 testdata
 attributes(testdata)
+
+# Add to package
 usethis::use_data(testdata, overwrite = TRUE)
