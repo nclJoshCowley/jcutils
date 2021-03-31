ab32062d272412fac1a33530023c64fffcd3f681
Josh Cowley <j.cowley1@ncl.ac.uk>
2021-03-31 17:10:28 +0100

    Remove private repo dependencies

--- tests/testthat/test-chol-solve.R
+++ tests/testthat/test-chol-solve.R
@@ -1,26 +1,18 @@
 context("Invert matrices with cholesky solve")
 
-test_that("chol_solve and solve consistent", {
-  x_test <- matrix(
-    c(20, 5, 8.5, 5, 17, 10.5, 8.5, 10.5, 19),
-    nrow = 3,
-    ncol = 3
-  )
+x_test <- structure(
+  c(20, 5, 8.5, 5, 17, 10.5, 8.5, 10.5, 19),
+  .Dim = c(3L, 3L)
+)
 
-  expect_equal(
-    solve(x_test),
-    SiteDataCollection::chol_solve(x_test)
-  )
+test_that("chol_solve and solve consistent", {
+  expect_equal(solve(x_test), jcutils::chol_solve(x_test))
 })
 
 test_that("chol_solve refuses asymmetric matrices", {
-  expect_error(
-    SiteDataCollection::chol_solve(matrix(1:4, 2, 2))
-  )
+  expect_error(jcutils::chol_solve(matrix(1:4, 2, 2)))
 })
 
 test_that("chol_solve refuses matrices with neagtive eigenvalues", {
-  expect_error(
-    SiteDataCollection::chol_solve(diag(c(-1, 1, 2)))
-  )
+  expect_error(jcutils::chol_solve(diag(c(-1, 1, 2))))
 })
