05c8377d673dfdc6d1127f24fdf643ce4bda4968
Josh Cowley <j.cowley1@ncl.ac.uk>
2021-12-15 14:10:39 +0000

    Moved chol_solve to base.R

--- R/base.R
+++ R/base.R
@@ -1,3 +1,31 @@
+# -------------------------------------------------------------------------|
+# Utilities that extend the usefulness of functions found in base.
+# -------------------------------------------------------------------------|
+
+
+#' Invert Symmetric Positive Definite Matrix via Cholesky Decomposition
+#'
+#' Consider a matrix that is symmetric and has positive eigenvalues, and is
+#'   therefore positive definite. Then finds the inverse of the cholesky
+#'   decomposition of this matrix as a more numerically stable alternative
+#'   to `base::solve(x)`.
+#'
+#' @param x matrix. symmetric (up to `tol`) and positive definite.
+#' @param tol numeric. Differences of symmetry smaller than this value
+#'   are not considered significant.
+#'
+#' @return the matrix inverse of `x`.
+#'
+#' @export
+chol_solve <- function(x, tol = 100 * .Machine$double.eps) {
+  stopifnot(
+    "x is not symmetric" = isSymmetric(x, tol),
+    "x is not positive definite" = all(eigen(x)$values > tol)
+  )
+  return(chol2inv(chol(x)))
+}
+
+
 #' Range Expander
 #'
 #' Modifies the output of \code{\link[base]{range}} to stretch or shrink.
