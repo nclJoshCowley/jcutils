0fa86e97b19c0b5f0bb8efe002b38d13d4a3b6da
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-01-15 14:20:35 +0000

    Removed (orphaned) suggest: bibtex

--- R/bibtex.R
+++ R/bibtex.R
@@ -1,76 +1,39 @@
-#' Collate BibTeX Entries
+#' Collate BibTeX Databases
 #'
 #' Simple function that will find all .bib files in a directory and combine them
-#'   into a single .bib file.
+#'   into a single `BibEntry` object.
 #'
-#' @param path character. The directory (and sub-directories) to search
-#'   for .bib files
-#' @inheritParams bibtex::write.bib
-#' @param overwrite logical. If TRUE, replaces 'file', if it already exists.
+#' @param path character. The directory to search recursively for .bib files.
 #'
-#' @return (invisible) An object of class "bibentry",
-#'   containing all found references.
+#' @seealso \code{\link[RefManageR]{ReadBib}}.
+#'
+#' @return \code{\link[RefManageR]{BibEntry}}.
 #'
 #' @export
-collate_bibtex <- function(path, file = "references.bib", overwrite = FALSE) {
-  # Remove file marked for overwrite
-  if (file.exists(file) & overwrite) unlink(file)
-  stopifnot("'file' already exists" = !file.exists(file))
-
-  # Load any .bib file
-  bib_db <- data.frame(
-    files = list.files(
+collate_bibtex <- function(path = ".") {
+  bib_paths <-
+    list.files(
       path = path,
       pattern = "*.bib",
       recursive = TRUE,
       full.names = TRUE
     )
-  )
-  stopifnot("Couldn't find any '*.bib' files" = (length(bib_db$files) > 0))
 
-  # Parse bib entries as a warning system for invalid files
-  withCallingHandlers(
-    warning = function(cnd) {
-      stop(
-        "Couldn't parse .bib files for following reasons:\n",
-        cnd,
-        call. = FALSE
-      )
-    },
-    bib_db$entry <- lapply(bib_db$files, bibtex::read.bib)
-  )
+  stopifnot("Couldn't find any '*.bib' files" = (length(bib_paths) > 0))
 
-  # Warn over duplicate keys
-  all_keys <- unlist(lapply(bib_db$entry, function(x) x$key))
-  if (anyDuplicated(all_keys)) {
-    dup_keys <- all_keys %in% all_keys[duplicated(all_keys)]
+  bib_list <- lapply(bib_paths, RefManageR::ReadBib)
+  bib_list_keys <- lapply(bib_list, function(.db) vapply(.db, names, character(1)))
+
+  bibs <- do.call(c, bib_list)
+  bibs_keys <- do.call(c, bib_list_keys)
+
+  if (anyDuplicated(bibs_keys)) {
     warning(
-      "Found duplicated keys.\n ",
-      paste0(all_keys[dup_keys], collapse = "\n "),
+      "Found duplicate keys:\n- ",
+      paste0(bibs_keys[duplicated(bibs_keys)], collapse = "\n- "),
       call. = FALSE
     )
   }
 
-  # To keep encoding and comments, join all files as text files
-  raw_text <- paste0(
-    unlist(lapply(
-      bib_db$files,
-      function(.x) paste0(readLines(.x, warn = FALSE), collapse = "\n")
-    )),
-    collapse = "\r\n\n"
-  )
-
-  writeLines(
-    paste0(
-      "% Generated using Collate-references.R. Do not edit by hand.\r\n\n",
-      raw_text
-    ),
-    "references.bib"
-  )
+  return(bibs)
 }
-
-
-
-
-
-
