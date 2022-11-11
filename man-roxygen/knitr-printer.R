053e43dd7421d8329237f59559019f9114f6d09c
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-11-11 15:15:11 +0000

    Added more detail to printer_tabset report usage
    Opting to use registerS3method over implicit `knit_print.foo <- *`

--- man-roxygen/knitr-printer.R
+++ man-roxygen/knitr-printer.R
@@ -4,8 +4,23 @@
 #' @family Knitr printer methods
 #'
 #' @section Reporting:
-#'   Either define a method, say `knit_print.foo <- printer_*`, in the report
-#'   or set the `render` chunk option equal to the desired function.
+#'   These methods are to be used in Quarto / RMD reports.
+#'
+#'   For global usage on all objects of class `foo`, register as an S3 method,
+#'   typically in the setup chunk.
+#'
+#'   ```r
+#'   library(knitr)
+#'   registerS3method("knit_print", "foo", printer_*)
+#'   ```
+#'
+#'   For one-off usage, set the `render` chunk option, that is:
+#'   ````markdown
+#'   ```{r}
+#'   #| render: !expr printer_*
+#'
+#'   ```
+#'   ````
 #'
 #' @seealso
 #'   - Generic documentation, [`knit_print`][knitr::knit_print].
